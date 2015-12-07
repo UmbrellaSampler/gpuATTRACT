e3da359249e1760bb15f6e8039cf95a766ca6393
zhezhang <zhe.zhang@tum.de>
2015-12-07 14:38:39 +0100

    wraped mc moves

--- mcATTRACT/src/mc_ensemble_move.f
+++ mcATTRACT/src/mc_ensemble_move.f
@@ -1,43 +1,58 @@
-      subroutine mc_ensemble_move(carstatehandle, nlig,
-     1 ens, phi, ssi, rot, xa, ya, za, scalecenter, scalerot,
-     2 ensprob, mover, cumu_sws)
+      subroutine mc_ensemble_move(cartstatehandle,nlig,fixre,
+     1 iori,itra,ens,nrens,ensprob,phi,ssi,rot,xa,ya,za,
+     2 scalecenter,scalerot,cumu_sws,mover,dseed)
 
       implicit none
       include 'max.fin'
-      integer nlig, dseed
-      real*8 phi, ssi, rot, xa, ya, za, cumu_sws
-      real*8 scalecenter, scalerot, rr, ens
-      dimension phi(maxlig), ssi(maxlig), rot(maxlig)
-      dimension xa(maxlig), ya(maxlig), za(maxlig)
-      dimension ens(maxlig)
-      dimension cumu_sws(maxmover)
-      dseed=12345
 
+C$$$      nlig: number of binding partners, it is 2 in 2-body docking
+C$$$      fixre: if receptor is fixed
+C$$$      itra: if translation is applied
+C$$$      iori: if orientation is changed, or rotation is applied
+C$$$      mover: applied mover index, needed later to count how many times rigid-body-mover applied
+C$$$      mover: 1 - rigid-body-mover alone, 2 - enstrans only, 3 - rigid-body-mover and enstrans simutaneously
+C$$$      ens: size maxlig, model index for each ligand(binding partner)
+C$$$      nrens: size maxlig, model number for each binding partner
+C$$$      scalecenter: magnitude for rigid-body translation
+C$$$      scalerot: magnitude for rigid-body rotation
+C$$$      cumu_sws: cumulative sampling weights for movers, options in the main code to define the sampling weights for each 3 movers
+C$$$      dseed: random number seed, it is neccessary to maintail the dseed in the main code, otherwise it will be needed to initialize each time here
+
+
+      integer cartstatehandle
+      integer nlig,fixre,itra,iori
+      integer i,mover,jl
+      integer ens,nrens
+
+      real*8 phi,ssi,rot,xa,ya,za,cumu_sws,dseed
+      real*8 scalecenter,scalerot,rr,ensprob
+
+      dimension ens(maxlig),nrens(maxlig)
+      dimension phi(maxlig),ssi(maxlig),rot(maxlig)
+      dimension xa(maxlig),ya(maxlig),za(maxlig)
+      dimension cumu_sws(maxmover),rr(maxdof)
+
+C$$$      dseed=12345
+
+      jl=3*iori*(nlig-fixre)
       call GGUBS(dseed,2,rr)
-
       call random_mover(rr(1),cumu_sws,mover,maxmover)
-c      print*,"mover ",mover
+      if (mover.eq.1 .or. mover.eq.3 .or. ensprob.eq.0) then
+         call rigid_body_mover(nlig,jl,iori,itra,phi,ssi,rot,xa,ya,za,
+     1        fixre,scalerot,scalecenter,dseed)
+      endif
+      if (mover.eq.2 .or. mover.eq.3) then
+         call GGUBS(dseed,3,rr)
+         do i=1,nlig
+            if (nrens(i).gt.0) then
 
-        if (mover.eq.1 .or. mover.eq.3 .or. ensprob.eq.0) then
-           trials = trials+1
-           call rigid_body_mover(nlig,jl,iori,itra,phi,ssi,rot,xa,ya,za,
-     1          fixre,scalerot,scalecenter,dseed)
-        endif
-        if (mover.eq.2 .or. mover.eq.3) then
-           call GGUBS(dseed,3,rr)
-           do i=1,nlig
-              if (nrens(i).gt.0) then
-
-                 if (rr(1).lt.ensprob.and.rr(3).lt.float(i)/nlig) then
-c	    ens(i) = int(rr(2)*nrens(i))+1
-                    call enstrans(cartstatehandle,i-1,ens(i),rr(2),
-     2                   ens(i))
-                    exit
-                 endif
-              endif
-           enddo
-        endif
+               if (rr(1).lt.ensprob.and.rr(3).lt.float(i)/nlig) then
+                  call enstrans(cartstatehandle,i-1,ens(i),rr(2),
+     2                 ens(i))
+                  exit
+               endif
+            endif
+         enddo
+      endif
 
       end
-
-
